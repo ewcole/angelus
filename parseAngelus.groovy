8c63cf3407b4ea835125ffd6d884249d6b85f267
Ed Cole <edcole@mail.sunyjcc.edu>
2013-05-10 12:51:00 -0400

    parseAngelus.groovy

--- parseAngelus.groovy
+++ parseAngelus.groovy
@@ -1,3 +1,4 @@
+import groovy.json.*
 
 /** Combine two data files (usually English and Latin) into one dual language
  *      version, which will be used to generate the HTML page.*/
@@ -9,23 +10,40 @@ assert opt
 def p = new File(opt.p)
 def s = new File(opt.s)
 def data = [
-lang: [
-  p: p.name.replaceAll(/.*\./, ''), 
-  s: s.name.replaceAll(/.*\./, '')
-],
-lines: [:]
+  lang: [
+    p: p.name.replaceAll(/.*\./, ''), 
+    s: s.name.replaceAll(/.*\./, '')
+  ],
+  lines: [:]
 ]
 println data
 [p,s].each {assert it.exists()}
-println "files exist"
 data.lines["p"] = p.readLines().grep {it =~ /[^ ]/}
 data.lines.s = s.readLines().grep {it =~ /[^ ]/}
 assert data.lines.p.size() == data.lines.s.size()
 // Now march through the lines and create unified file.
 def l = data.lines
+println "data.lang=${data.lang}"
 def sections = (0 .. data.lines.p.size()).collect {
   i ->
     def pt = l.p[i];
     def st = l.s[i];
-    assert pt =~ /^([a-zA-Z]+): *(.*)/
+    println "data.lang=${data.lang}"
+    def lang=data.lang
+    (pt =~ /&(.*)\.\.\. *$/).each {
+      m ->
+        def fileBase = m[1]
+        ptf = new File("$fileBase.${lang.p}")
+        stf = new File("$fileBase.${lang.s}")
+        pt = "$fileBase: ${ptf.text}"
+        st = "$fileBase: ${stf.text}"
+    }
+    def sct = ['','','']
+    (pt =~ /^([a-zA-Z]+): *(.*)/).each {
+      sct = [it[1] as String, 
+             pt.replaceAll(/^[^:]+: */,''), 
+             st.replaceAll(/^[^:]+: */,'')]
+    }
+    sct
 }
+println new JsonOutput().prettyPrint(new JsonOutput().toJson(sections))
